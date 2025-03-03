3ddb40ab5cc1931a9b10ce80f4b0eabece511a1f
Mila-Amen <million.degefu@dci-student.org>
2025-03-03 10:06:00 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Nav from "./components/Nav.jsx"
+
+function App() {
+  return (
+    <>
+    <Nav/>
+      <h1 className="text-3xl font-bold underline">    Biniam Garage!</h1>
+    </>
+  );
+}
+
+export default App;
